402648962203a0dc37cf73cbe23993e23f355758
AndrePassoni <andre.magalhaes.passoni@gmail.com>
2026-07-07 14:19:44 -0300

    feat: add Tip component for game hints

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import styles from "./app.module.css"
 
 import { Header } from "./components/Header"
+import { Tip } from "./components/Tip"
 
 export default function() {
   function handleRestartGame() {
@@ -11,6 +12,7 @@ export default function() {
     <div className={styles.container}>
       <main>
         <Header current={5} max={10} onRestart={handleRestartGame} />
+        <Tip tip="Linguagem de Programação Dinâmica"/>
       </main>
     </div>
   )
