292f75e71aee9d767caca1e5a5a2280a9888787a
AndrePassoni <andre.magalhaes.passoni@gmail.com>
2026-07-07 09:58:43 -0300

    feat: add initial Adivinhe React game

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import styles from "./app.module.css"
+
+import { Header } from "./components/Header"
+
+export default function() {
+  return (
+    <div className={styles.container}>
+      <main>
+        <Header />
+      </main>
+    </div>
+  )
+}
