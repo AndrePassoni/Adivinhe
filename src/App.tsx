675cc0147771dcda044b5f49a256cea69ad1f633
AndrePassoni <andre.magalhaes.passoni@gmail.com>
2026-07-07 20:34:47 -0300

    feat: add Button component and integrate it into the main view; enhance styles for guess input

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Header } from "./components/Header"
 import { Letter } from "./components/Letter"
 import { Input } from "./components/Input"
 import { Tip } from "./components/Tip"
+import { Button } from "./components/Button"
 
 export default function() {
   function handleRestartGame() {
@@ -26,8 +27,9 @@ export default function() {
 
         <h4>Palpite</h4>
 
-        <div>
+        <div className={styles.guess}>
           <Input autoFocus maxLength={1} placeholder="?" />
+          <Button title="Confirmar" />
         </div>
       </main>
     </div>
