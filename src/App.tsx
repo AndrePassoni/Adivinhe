a891216d648a711beb29abc0f7a5ea0b562766b9
AndrePassoni <andre.magalhaes.passoni@gmail.com>
2026-07-08 15:39:50 -0300

    fix: update restart game confirmation to prompt user before restarting

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,11 @@ export default function() {
   const [challenge, setChallenge] = useState<Challenge | null>(null)
 
   function handleRestartGame() {
-    alert("Reiniciar o jogo!")
+    const isConfirmed = window.confirm("Você tem certeza que deseja reiniciar?")
+
+    if (isConfirmed) {
+      startGame()
+    }
   }
 
   function startGame() {
