455b1576f5f244902e3c6181291ba60f3bf93d94
AndrePassoni <andre.magalhaes.passoni@gmail.com>
2026-07-07 22:27:52 -0300

    feat: implement game logic with state management; initialize game on component mount

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
 import styles from "./app.module.css"
+import { useEffect, useState } from "react"
+
+import { WORDS } from "./utils/words"
+import type { Challenge } from "./utils/words"
 
 import { Header } from "./components/Header"
 import { Letter } from "./components/Letter"
@@ -8,22 +12,44 @@ import { Button } from "./components/Button"
 import { LettersUsed } from "./components/Letters"
 
 export default function() {
+  const [letter, setLetter] = useState("")
+  const [attempts, setAttempts] = useState(0)
+  const [challenge, setChallenge] = useState<Challenge | null>(null)
+
   function handleRestartGame() {
     alert("Reiniciar o jogo!")
+  }
+
+  function startGame() {
+    const index = Math.floor(Math.random() * WORDS.length)
+    const randomWord = WORDS[index]
+    
+    setChallenge(randomWord)
+
+    setAttempts(0)
+    setLetter("")
+  }
+
+  useEffect(() => {
+    startGame()
+  }, [])
+
+  if(!challenge) {
+    return
   }
 
   return (
     <div className={styles.container}>
       <main>
-        <Header current={5} max={10} onRestart={handleRestartGame} />
+        <Header current={attempts} max={10} onRestart={handleRestartGame} />
         <Tip tip="Linguagem de Programação Dinâmica"/>
 
         <div className={styles.word}>
-          <Letter value="R"/>
-          <Letter value="E"/>
-          <Letter value="A"/>
-          <Letter value="C"/>
-          <Letter value="T"/>
+          {
+            challenge.word.split("").map(() => (
+              <Letter value=""/>
+            ))
+          }
         </div>
 
         <h4>Palpite</h4>
